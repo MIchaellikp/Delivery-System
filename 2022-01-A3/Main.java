cfeea50580b5a809d2bd0c30fe2158979ec5ee7c
Kunpeng Li <kxl465@gatech.edu>
2022-04-11 20:22:01 -0500

    Add SQL Code

--- 2022-01-A3/Main.java
+++ 2022-01-A3/Main.java
@@ -1,9 +1,47 @@
-import edu.gatech.cs6310.*;
+import edu.gatech.cs6310.DeliveryService;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
+    private static Connection con;
+    //private static DriverManagerDataSource ds;
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/delivery";
+            String userName = "root";
+            String password = "1234";
+            con = DriverManager.getConnection(url, userName, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Grocery Express Delivery Service!");
+        System.out.println("Please Login");
+        Scanner username = new Scanner(System.in);
+        System.out.println("Please Password");
+        Scanner password = new Scanner(System.in);
+        //find the User or password
+        try {
+            Statement state = con.createStatement();
+            String sql = "Select * from Users where username = " + username + "and password = " + password;
+            ResultSet rs = state.executeQuery(sql);
+            if(rs.next()){
+                //execute the system
+            }else{
+                //recall the Name & Password
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         DeliveryService simulator = new DeliveryService();
         simulator.commandLoop();
     }
---
+++ 2022-01-A3/edu/gatech/cs6310/SQL/DeliverySystem.sql
@@ -0,0 +1,123 @@
+Use DeliveryService;
+
+drop table if exists itemLine;
+drop table if exists items;
+drop table if exists Orders;
+drop table if exists Customers;
+drop table if exists Drones;
+drop table if exists Stores;
+drop table if exists Pilots;
+drop table if exists Users;
+drop table if exists System_Log;
+
+create table Users
+(
+	username varchar(100) not null,
+    password varchar(100) not null,
+    primary key(username)
+);
+
+create table System_Log
+(
+	logID int not null auto_increment,
+    username varchar(100) not null,
+    commandLine varchar(100) not null,
+    resultMessage varchar(100) not null,
+    timeStamp datetime,
+    primary key(logID)
+);
+
+create table Customers
+(
+   customerID varchar(100) not null,
+   rating int not null,
+   credits int not null,
+   remainingCredit int not null,
+   timeStamp datatime,
+   primary key (customerID)
+);
+
+create table Stores
+(
+	storeName varchar(100) not null,
+	revenue int not null,
+    timeStamp datatime,
+	primary key (storeName)
+);
+
+create table Orders
+( 
+	orderID varchar(100) not null,
+	droneID varchar(100) not null,
+    totalCost int not null,
+    totalWeight int not null,
+    customerID varchar(100) not null,
+    status varchar(100) not null,
+    timeStamp datatime,
+    primary key(orderID)
+);
+
+create table Drones
+(
+	droneID varchar(100) not null,
+    storeName varchar(100) not null,
+    capacity int not null,
+    remainCap int not null,
+    numsOrders int not null,
+    fuel int not null,
+    remainFuel int not null,
+    pilotID varchar(100) not null,
+    timeStamp datatime,
+    primary key(droneID)
+);
+
+create table items
+(
+	itemName varchar(100) not null,
+    storeName varchar(100) not null,
+    price int not null,
+    weight int not null,
+    primary key(itemName, storeName)
+);
+
+create table itemLine
+(	
+	orderId varchar(100) not null,
+    itemName varchar(100) not null,
+    totalQuantity int not null,
+    totalCost int not null,
+    timeStamp datatime,
+    primary key(orderId, itemName)
+);
+
+create table Pilots
+(
+	accountID varchar(100) not null,
+    firstName varchar(100) not null,
+    lastName varchar(100) not null,
+    phoneNumber varchar(100) not null,
+    taxID varchar(100) not null,
+    licenseID varchar(100) not null,
+    expcLevel int not null,
+    droneID varchar(100),
+    timeStamp datatime,
+    primary key(accountID),
+    unique key AK_nq_licenseID(licenseID)
+);
+
+alter table Orders add constraint FK_order_drone foreign key (droneID)
+      references Drones(droneID) on delete restrict on update restrict;
+alter table Orders add constraint FK_order_customer foreign key (customerID)
+      references Customers (customerID) on delete restrict on update restrict;
+alter table Drones add constraint FK_drone_store foreign key (storeName)
+      references Stores (storeName) on delete restrict on update restrict;
+alter table Drones add constraint FK_drone_pilots foreign key (pilotID)
+      references Pilots (accountID) on delete restrict on update restrict;
+alter table items add constraint FK_item_store foreign key (storeName)
+      references Stores (storeName) on delete restrict on update restrict;
+alter table itemLine add constraint FK_itemLine_order foreign key (orderID)
+      references Orders (orderID) on delete restrict on update restrict;
+alter table itemLine add constraint FK_itemLine_item foreign key (itemName)
+      references Items (itemName) on delete restrict on update restrict;    
+alter table Pilots add constraint FK_pilot_drone foreign key (droneID)
+      references Drones (droneID) on delete restrict on update restrict;
