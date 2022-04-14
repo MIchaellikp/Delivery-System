847aa325db7cba9a2df32c97bbfc16acede1cfa0
gyen6 <gyen6@gatech.edu>
2022-04-14 15:24:14 -0500

    update mysql driver jar and connection initialization

--- 2022-01-A3/Main.java
+++ 2022-01-A3/Main.java
@@ -1,9 +1,12 @@
 import edu.gatech.cs6310.DeliveryService;
 import edu.gatech.cs6310.Init;
-import edu.gatech.cs6310.SQL.SQLtools;
+import edu.gatech.cs6310.SQL.logTool;
 //import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.Scanner;
 
@@ -11,41 +14,45 @@ public class Main {
     private static Connection con;
     //private static DriverManagerDataSource ds;
 
-    static {
+    private static void initConenction() throws SQLException{
+        final String url = "jdbc:mysql://localhost:3306/delivery";
+        final String userName = "demo_java";
+        final String password = "1234";
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/delivery";
-            String userName = "root";
-            String password = "1234";
+//            Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(url, userName, password);
-        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Connection established!");
+//        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
+            throw e;
         }
-
     }
     //todo - create tables
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, SQLException, ClassNotFoundException {
+        initConenction();
+
         System.out.println("Welcome to the Grocery Express Delivery Service!");
         System.out.println("Please Login");
         Scanner username = new Scanner(System.in);
         System.out.println("Please Password");
         Scanner password = new Scanner(System.in);
         //find the User or password
-        try {
-            SQLtools st = new SQLtools(con);
-
-            Statement state = con.createStatement();
-            String sql = "Select * from Users where username = " + username + "and password = " + password;
-            ResultSet rs = state.executeQuery(sql);
-            if(rs.next()){
-                //execute the system
-            }else{
-                //recall the Name & Password
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            SQLtools st = new SQLtools(con);
+//
+//            Statement state = con.createStatement();
+//            String sql = "Select * from Users where username = " + username + "and password = " + password;
+//            ResultSet rs = state.executeQuery(sql);
+//            if(rs.next()){
+//                //execute the system
+//            }else{
+//                //recall the Name & Password
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
 
         // add into userName
         DeliveryService simulator = new DeliveryService();
